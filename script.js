966fbe5d923757cdaa60eb223443f78e5af18a2d
Eric Kiverenge <avwengae@gmail.com>
2024-05-09 21:15:14 +0300

    Added home and about section

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   
     // Preload the actual background image
     var img = new Image();
-    img.src = 'Images/background2.jpg'; // Replace with the URL of your actual background image
+    img.src = 'Images/background.jpg'; // Replace with the URL of your actual background image
   
     // When the actual image is loaded, replace the placeholder with it
     img.onload = function() {
